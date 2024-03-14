1c5fe94345abb31c0c24915dcdca8c6d20ed9f08
Julia Martins <juliamartins.td2.senai@gmail.com>
2024-03-14 10:41:35 -0300

    coloquei em ordem crescente

--- aulaarray.js
+++ aulaarray.js
@@ -4,3 +4,8 @@
 let num = [5, 8, 3, 4, 7]
 console.log("--- exibir Array ---");
 console.log(num)
+
+//colocar em ordem crescente
+console.log ("---exibir em ordem crescente---");
+num.sort()
+console.log(num)
